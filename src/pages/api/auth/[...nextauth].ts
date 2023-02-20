65568e0514284d95754b8157bbb40a9a23acaa79
peterleunan1 <arpm081001@gmail.com>
2023-02-20 11:38:44 -0500

    fIX: Adding secre ...nextAuth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -8,6 +8,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_SECRET || "",
     }),
   ],
+  secret: process.env.JWT_SECRET || "",
 };
 
 export default NextAuth(authOptions);
